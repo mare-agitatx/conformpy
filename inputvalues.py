c88a830d0807a0ab04182c5c690a6b3e3e7eccaf
Mare <mare.agitatx@gmail.com>
2024-08-12 09:54:30 +0200

    added numpy call in order to define math constants and functions directly into the input module

--- inputvalues.py
+++ inputvalues.py
@@ -1,10 +1,12 @@
-GRAPH_TITLE = 'f(z) = z**3'
-X_GRID_START = -8.
-X_GRID_STOP = 8.
-X_GRID_STEP = 2.
-Y_GRID_START = -8.
-Y_GRID_STOP = 8.
-Y_GRID_STEP = 2.
+import numpy as np
+# input values for the algorithm
+GRAPH_TITLE = 'f(z) = z**3 +z**2'
+X_GRID_START = -10
+X_GRID_STOP = 10
+X_GRID_STEP = .2
+Y_GRID_START = -10
+Y_GRID_STOP = 10
+Y_GRID_STEP = .2
 RENDER_SAMPLE_SIZE = 200
 IS_THE_IDENTITY_GRID_SHOWN = False
 ARE_THE_REAL_CURVES_SHOWN = True
@@ -13,4 +15,4 @@ ALPHA_VALUE = 0.75
 
 
 def complex_function_formula(z):
-    return (z**3)
+    return (z**3 +z**2)
